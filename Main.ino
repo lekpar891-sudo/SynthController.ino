b050bf02efb06cf318404bc316ff85310c5579e2
lekpar891-sudo <lekpar891@gmail.com>
2026-01-01 17:13:58 +0700

    Improve USB MIDI handling in Main.ino
    Refactor USB MIDI initialization and polling logic.

--- Main.ino
+++ Main.ino
@@ -1,16 +1,13 @@
 #include <Arduino.h>
 #include <USBComposite.h>
 
-// Inisialisasi USB MIDI
 USBMIDI usbMIDI;
 
-/* ---------- KONFIGURASI PIN ---------- */
 const uint8_t ROW_PINS[8] = {PA0, PA1, PA2, PA3, PA4, PA5, PA6, PA7};
 const uint8_t COL_PINS[8] = {PB0, PB1, PB2, PB10, PB11, PB12, PB13, PB14};
 uint8_t matrixState[8][8] = {0};
 
 void setup() {
-  // Set agar STM32 terbaca sebagai MIDI Device
   USBComposite.setProductId(0x0030);
   usbMIDI.begin();
   USBComposite.begin();
@@ -22,8 +19,7 @@ void setup() {
 }
 
 void loop() {
-  // Wajib panggil poll agar USB tetap aktif
-  usbMIDI.poll();
+  usbMIDI.poll(); // Sangat penting agar USB tetap konek
 
   for (int c = 0; c < 8; c++) {
     pinMode(COL_PINS[c], OUTPUT);
@@ -34,7 +30,7 @@ void loop() {
         matrixState[r][c] = pressed;
         uint8_t note = 48 + (r * 8 + c); 
         if (pressed) {
-          usbMIDI.sendNoteOn(0, note, 100); // Channel 1, Note, Velocity
+          usbMIDI.sendNoteOn(0, note, 100);
         } else {
           usbMIDI.sendNoteOff(0, note, 0);
         }
