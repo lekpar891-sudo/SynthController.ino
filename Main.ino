06d764efc120cb7a0126ee28cd801347e87536a2
lekpar891-sudo <lekpar891@gmail.com>
2026-01-01 17:19:32 +0700

    Enhance comments in Main.ino for better understanding
    Added comments for clarity and improved code readability.

--- Main.ino
+++ Main.ino
@@ -3,11 +3,13 @@
 
 USBMIDI usbMIDI;
 
+// Pin Matrix (Sesuaikan dengan skema Anda)
 const uint8_t ROW_PINS[8] = {PA0, PA1, PA2, PA3, PA4, PA5, PA6, PA7};
 const uint8_t COL_PINS[8] = {PB0, PB1, PB2, PB10, PB11, PB12, PB13, PB14};
 uint8_t matrixState[8][8] = {0};
 
 void setup() {
+  // Inisialisasi USB sebagai Perangkat MIDI
   USBComposite.setProductId(0x0030);
   usbMIDI.begin();
   USBComposite.begin();
@@ -19,18 +21,20 @@ void setup() {
 }
 
 void loop() {
-  usbMIDI.poll(); // Sangat penting agar USB tetap konek
+  usbMIDI.poll(); // Wajib ada agar koneksi USB tetap terjaga
 
   for (int c = 0; c < 8; c++) {
     pinMode(COL_PINS[c], OUTPUT);
     digitalWrite(COL_PINS[c], LOW);
+    
     for (int r = 0; r < 8; r++) {
       bool pressed = (digitalRead(ROW_PINS[r]) == LOW);
       if (pressed != matrixState[r][c]) {
         matrixState[r][c] = pressed;
-        uint8_t note = 48 + (r * 8 + c); 
+        uint8_t note = 48 + (r * 8 + c); // Nada dasar C3
+        
         if (pressed) {
-          usbMIDI.sendNoteOn(0, note, 100);
+          usbMIDI.sendNoteOn(0, note, 100); // Kirim ke Android
         } else {
           usbMIDI.sendNoteOff(0, note, 0);
         }
