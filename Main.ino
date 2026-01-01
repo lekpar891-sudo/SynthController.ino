c993a677e3fa12a13b4b48f3eed7fad5946b6e6a
lekpar891-sudo <lekpar891@gmail.com>
2026-01-01 17:09:17 +0700

    Switch to USB MIDI for note handling
    Refactor MIDI handling to use USB MIDI library and remove Serial MIDI code.

--- Main.ino
+++ Main.ino
@@ -1,15 +1,19 @@
 #include <Arduino.h>
+#include <USBComposite.h>
 
-#define MIDI_HW Serial1 // MIDI Out Fisik (PA9)
+// Inisialisasi USB MIDI
+USBMIDI usbMIDI;
 
+/* ---------- KONFIGURASI PIN ---------- */
 const uint8_t ROW_PINS[8] = {PA0, PA1, PA2, PA3, PA4, PA5, PA6, PA7};
 const uint8_t COL_PINS[8] = {PB0, PB1, PB2, PB10, PB11, PB12, PB13, PB14};
 uint8_t matrixState[8][8] = {0};
 
 void setup() {
-  // Dengan opsi usb=MIDI di YAML, Serial ini otomatis jadi USB MIDI
-  Serial.begin(115200);
-  MIDI_HW.begin(31250);
+  // Set agar STM32 terbaca sebagai MIDI Device
+  USBComposite.setProductId(0x0030);
+  usbMIDI.begin();
+  USBComposite.begin();
 
   for (int i = 0; i < 8; i++) {
     pinMode(ROW_PINS[i], INPUT_PULLUP);
@@ -18,6 +22,9 @@ void setup() {
 }
 
 void loop() {
+  // Wajib panggil poll agar USB tetap aktif
+  usbMIDI.poll();
+
   for (int c = 0; c < 8; c++) {
     pinMode(COL_PINS[c], OUTPUT);
     digitalWrite(COL_PINS[c], LOW);
@@ -27,17 +34,12 @@ void loop() {
         matrixState[r][c] = pressed;
         uint8_t note = 48 + (r * 8 + c); 
         if (pressed) {
-          // Kirim Note On (Channel 1)
-          Serial.write(0x90); Serial.write(note); Serial.write(100);
-          MIDI_HW.write(0x90); MIDI_HW.write(note); MIDI_HW.write(100);
+          usbMIDI.sendNoteOn(0, note, 100); // Channel 1, Note, Velocity
         } else {
-          // Kirim Note Off
-          Serial.write(0x80); Serial.write(note); Serial.write(0);
-          MIDI_HW.write(0x80); MIDI_HW.write(note); MIDI_HW.write(0);
+          usbMIDI.sendNoteOff(0, note, 0);
         }
       }
     }
     pinMode(COL_PINS[c], INPUT);
   }
-  delay(5);
 }
