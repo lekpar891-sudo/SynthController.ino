28b7fe819ffb342136caaf8b6a24558c2583f6d4
lekpar891-sudo <lekpar891@gmail.com>
2026-01-02 07:23:01 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,45 @@
 #include <Arduino.h>
 #include <USBComposite.h>
 
-// Inisialisasi USB MIDI
 USBMIDI usbMIDI;
 
-/* ---------- KONFIGURASI PIN ---------- */
-// Row PA0-PA7, Col PB0,PB1,PB2,PB10-PB14 (Sesuai kode awal Anda)
+// Konfigurasi Pin (Sesuai skema Anda)
 const uint8_t ROW_PINS[8] = {PA0, PA1, PA2, PA3, PA4, PA5, PA6, PA7};
 const uint8_t COL_PINS[8] = {PB0, PB1, PB2, PB10, PB11, PB12, PB13, PB14};
 uint8_t matrixState[8][8] = {0};
 
 void setup() {
-  // Setup USB sebagai MIDI Device
-  USBComposite.setProductId(0x0030);
-  usbMIDI.begin();
-  USBComposite.begin();
+    // Inisialisasi USB sebagai MIDI Device
+    USBComposite.setProductId(0x0030);
+    usbMIDI.begin();
+    USBComposite.begin();
 
-  // Setup Pin Matrix
-  for (int i = 0; i < 8; i++) {
-    pinMode(ROW_PINS[i], INPUT_PULLUP);
-    pinMode(COL_PINS[i], INPUT);
-  }
+    for (int i = 0; i < 8; i++) {
+        pinMode(ROW_PINS[i], INPUT_PULLUP);
+        pinMode(COL_PINS[i], INPUT);
+    }
 }
 
 void loop() {
-  // Wajib dipanggil agar koneksi USB tetap aktif
-  usbMIDI.poll();
+    usbMIDI.poll(); // Menjaga koneksi USB agar tidak terputus
 
-  // Scan Matrix 8x8
-  for (int c = 0; c < 8; c++) {
-    pinMode(COL_PINS[c], OUTPUT);
-    digitalWrite(COL_PINS[c], LOW);
-    
-    for (int r = 0; r < 8; r++) {
-      bool pressed = (digitalRead(ROW_PINS[r]) == LOW);
-      if (pressed != matrixState[r][c]) {
-        matrixState[r][c] = pressed;
-        uint8_t note = 48 + (r * 8 + c); // C3 sebagai nada dasar
+    for (int c = 0; c < 8; c++) {
+        pinMode(COL_PINS[c], OUTPUT);
+        digitalWrite(COL_PINS[c], LOW);
         
-        if (pressed) {
-          usbMIDI.sendNoteOn(0, note, 100); // Channel 1, Note, Vel 100
-        } else {
-          usbMIDI.sendNoteOff(0, note, 0);
+        for (int r = 0; r < 8; r++) {
+            bool pressed = (digitalRead(ROW_PINS[r]) == LOW);
+            if (pressed != matrixState[r][c]) {
+                matrixState[r][c] = pressed;
+                uint8_t note = 48 + (r * 8 + c);
+                
+                if (pressed) {
+                    usbMIDI.sendNoteOn(0, note, 100);
+                } else {
+                    usbMIDI.sendNoteOff(0, note, 0);
+                }
+            }
         }
-      }
+        pinMode(COL_PINS[c], INPUT);
     }
-    pinMode(COL_PINS[c], INPUT);
-  }
 }
