f07bd2a069f0a541c9e13e06a9ef052615600ddc
lekpar891-sudo <lekpar891@gmail.com>
2026-01-25 12:02:46 +0700

    Refactor configuration and logic for joystick control

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include <USBComposite.h>
 
-// --- KONFIGURASI 61 KEYS (SESUAI KODE ANDA - JANGAN DIUBAH) ---
+// --- KONFIGURASI 61 KEYS (DARI KODE ANDA) ---
 #define CLOCK_ENABLE_PIN PB4
 #define PARALLEL_LOAD_PIN PB7
 #define CLOCK_PIN PB6
@@ -13,24 +13,26 @@
 #define DEBOUNCING_CYCLES 128
 byte keyStates[NOTES_COUNT] = {0};
 
-// --- KONFIGURASI PANEL KONTROL (INTRO, VAR, START/STOP, KEY SET) ---
-// Pin PA0 s/d PA9 (Total 10 tombol). Hubungkan ke tombol lalu ke GND.
-const int panelPins[] = {PA0, PA1, PA2, PA3, PA4, PA5, PA6, PA7, PA8, PA9};
-const int numPanel = 10;
-// Mapping: 1-3 Intro, 4-7 Var, 8 Start/Stop, 9-10 Key Set
+// --- KONFIGURASI ANALOG PS ---
+#define ANALOG_X_PIN PA4
+#define ANALOG_Y_PIN PA5
 
-// --- KONFIGURASI ROTARY ENCODER (TEMPO) ---
+// --- KONFIGURASI PANEL KONTROL (Intro, Var, Start/Stop) ---
+const int panelPins[] = {PA0, PA1, PA2, PA3, PA6, PA7, PB12, PB13, PB14, PB15};
+const int numPanel = 10;
+
+// --- KONFIGURASI ROTARY ENCODER ---
 #define ENC_A PB0
 #define ENC_B PB1
 int lastEncoded = 0;
 
-// --- INISIALISASI USB COMPOSITE (MIDI + JOYSTICK) ---
+// USB Objects
 USBHID HID;
 HIDJoystick Joystick(HID);
 USBMIDI midi;
 
 void setup() {
-  // Setup Pin 61 Keys (Persis Kode Anda)
+  // Setup Pins 61 Keys
   pinMode(CLOCK_ENABLE_PIN, OUTPUT);
   pinMode(PARALLEL_LOAD_PIN, OUTPUT);
   pinMode(CLOCK_PIN, OUTPUT);
@@ -38,39 +40,33 @@ void setup() {
   digitalWrite(PARALLEL_LOAD_PIN, HIGH);
   digitalWrite(CLOCK_ENABLE_PIN, HIGH);
 
-  // Setup Pin Panel Kontrol (Joystick Buttons)
-  for(int i=0; i<numPanel; i++) {
-    pinMode(panelPins[i], INPUT_PULLUP);
-  }
+  // Setup Panel & Analog
+  for(int i=0; i<numPanel; i++) pinMode(panelPins[i], INPUT_PULLUP);
+  pinMode(ANALOG_X_PIN, INPUT_ANALOG);
+  pinMode(ANALOG_Y_PIN, INPUT_ANALOG);
 
-  // Setup Pin Rotary Encoder
+  // Setup Encoder
   pinMode(ENC_A, INPUT_PULLUP);
   pinMode(ENC_B, INPUT_PULLUP);
 
-  // Memulai USB Composite
-  USBComposite.setProductId(0x0031); // ID Perangkat
+  // Start USB
+  USBComposite.setProductId(0x0031);
   midi.begin();
   Joystick.begin();
   USBComposite.begin();
 }
 
 void loop() {
-  // --- BAGIAN 1: LOGIKA 61 KEYS (PERSIS KODE ASLI ANDA) ---
+  // --- 1. LOGIKA 61 KEYS (Kirim MIDI Note) ---
   digitalWrite(PARALLEL_LOAD_PIN, LOW);
   delayMicroseconds(5);
   digitalWrite(PARALLEL_LOAD_PIN, HIGH);
-  delayMicroseconds(5);
-
+  
   for (int r = 0; r < REGISTRIES_COUNT; r++) {
     digitalWrite(CLOCK_PIN, HIGH);
-    delayMicroseconds(5);
     digitalWrite(CLOCK_ENABLE_PIN, LOW);
-    delayMicroseconds(5);
-    
     byte incoming = shiftIn(SERIAL_DATA_PIN, CLOCK_PIN, MSBFIRST);
-    
     digitalWrite(CLOCK_ENABLE_PIN, HIGH);
-    delayMicroseconds(5);
 
     int registryOffset = r * 8;
     int lastRegistryNote = (r == (REGISTRIES_COUNT - 1)) ? LAST_REGISTRY_NOTE : 8;
@@ -78,45 +74,37 @@ void loop() {
     for (int i = 0; i < lastRegistryNote; i++) {
       byte* keyState = &keyStates[registryOffset + i];
       bool isCurrentlyPressed = incoming & (1 << i);
-      bool wasPressedBefore = *keyState;
-
-      if (!wasPressedBefore && isCurrentlyPressed) {
+      if (!(*keyState) && isCurrentlyPressed) {
         midi.sendNoteOn(0, FIRST_NOTE + registryOffset + i, 127);
         *keyState = DEBOUNCING_CYCLES;
       }
-
-      if (wasPressedBefore && !isCurrentlyPressed) {
+      if ((*keyState) && !isCurrentlyPressed) {
         if (*keyState > 0) *keyState = *keyState - 1;
-        if (*keyState == 0) {
-          midi.sendNoteOff(0, FIRST_NOTE + registryOffset + i, 127);
-        }
+        if (*keyState == 0) midi.sendNoteOff(0, FIRST_NOTE + registryOffset + i, 127);
       }
     }
   }
 
-  // --- BAGIAN 2: LOGIKA PANEL KONTROL (DIUBAH KE JOYSTICK BUTTONS) ---
-  // Ini menggantikan logika Joystick Diego Viejo agar sesuai dengan tombol fisik
+  // --- 2. LOGIKA ANALOG PS (Kirim Joystick Axis) ---
+  // Membaca analog dan merubah rentang ke standar Joystick (-32767 sampai 32767)
+  int valX = analogRead(ANALOG_X_PIN);
+  int valY = analogRead(ANALOG_Y_PIN);
+  Joystick.X(map(valX, 0, 4095, -32767, 32767));
+  Joystick.Y(map(valY, 0, 4095, -32767, 32767));
+
+  // --- 3. LOGIKA PANEL KONTROL (Kirim Joystick Buttons) ---
   for (int j = 0; j < numPanel; j++) {
-    if (digitalRead(panelPins[j]) == LOW) {
-      Joystick.button(j + 1, 1); // Tekan tombol joystick
-    } else {
-      Joystick.button(j + 1, 0); // Lepas tombol joystick
-    }
+    Joystick.button(j + 1, !digitalRead(panelPins[j]));
   }
 
-  // --- BAGIAN 3: LOGIKA ROTARY ENCODER (TEMPO) ---
+  // --- 4. LOGIKA ROTARY ENCODER (Kirim MIDI CC Tempo) ---
   int MSB = digitalRead(ENC_A);
   int LSB = digitalRead(ENC_B);
   int encoded = (MSB << 1) | LSB;
   int sum = (lastEncoded << 2) | encoded;
-
-  if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) {
-    midi.sendControlChange(0, 20, 1); // CC 20 untuk Tempo Up
-  }
-  if(sum == 0b1110 || sum == 0b1000 || sum == 0b0001 || sum == 0b0111) {
-    midi.sendControlChange(0, 20, 127); // CC 20 untuk Tempo Down
-  }
+  if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) midi.sendControlChange(0, 20, 1);
+  if(sum == 0b1110 || sum == 0b1000 || sum == 0b0001 || sum == 0b0111) midi.sendControlChange(0, 20, 127);
   lastEncoded = encoded;
 
-  delayMicroseconds(100); 
+  delayMicroseconds(200);
 }
